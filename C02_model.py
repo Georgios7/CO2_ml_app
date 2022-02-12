a7942acd6b0993d2728a5493c7e8ee99c7f0d8b3
George <gchatzinis@gmail.com>
2022-02-12 22:50:00 +0100

    added features

--- C02_model.py
+++ C02_model.py
@@ -3,13 +3,13 @@ import pandas as pd
 from pycaret.regression import *
 
 data = pd.read_csv('CO2 Emissions_Canada.csv')
-data = data[["Vehicle Class", "Cylinders","Transmission","Fuel Type",
-"Fuel Consumption City (L/100 km)", "Fuel Consumption Hwy (L/100 km)", "CO2 Emissions(g/km)"]]
+data = data[["Vehicle Class","Engine Size(L)", "Cylinders","Transmission","Fuel Type",
+"Fuel Consumption City (L/100 km)", "Fuel Consumption Hwy (L/100 km)","Fuel Consumption Comb (L/100 km)" ,"CO2 Emissions(g/km)"]]
 
 models_setup = setup(data, target = "CO2 Emissions(g/km)", session_id = 123)
 
-# Gradient boosting reggressor
-gbr = create_model('gbr')
-tuned_gbr =  tune_model(gbr)
+# Random forrest reggressor
+rf = create_model('rf')
+#tuned_rf =  tune_model(rf)
 
-save_model(tuned_gbr,model_name = 'deployment_CO2')
+save_model(rf,model_name = 'deployment_CO2')
--- CO2_App.py
+++ CO2_App.py
@@ -1,4 +1,4 @@
-# How to run: Open command propmpt and run the command: streamlit runn app.py
+# How to run: Open command propmpt and run the command: streamlit run app.py
 
 from pycaret.regression import load_model, predict_model
 from sqlalchemy import false
@@ -6,14 +6,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# data descriptions
-
-#data_des = pd.read_csv('Data Description.csv')
 
 # Load Model
 model = load_model('deployment_CO2')
 
 def run():
+
+    # Use the full page instead of a narrow central column
+    st.set_page_config( page_title='C02 car emmision app' , page_icon="🌳", layout="centered", 
+    initial_sidebar_state="auto", menu_items=None)
+
+
 
     from PIL import Image
     image_CO2 = Image.open('CO2.jpg')
@@ -44,29 +47,36 @@ def run():
        'PICKUP TRUCK - STANDARD', 'MINIVAN', 'SPECIAL PURPOSE VEHICLE',
        'STATION WAGON - MID-SIZE', 'PICKUP TRUCK - SMALL']))
 
-    Transmition = st.selectbox('Transmission',['AS5', 'M6', 'AV7', 'AS6', 'AM6', 'A6', 'AM7', 'AV8', 'AS8', 'A7',
-       'A8', 'M7', 'A4', 'M5', 'AV', 'A5', 'AS7', 'A9', 'AS9', 'AV6',
-       'AS4', 'AM5', 'AM8', 'AM9', 'AS10', 'A10', 'AV10'])
+    engine_size = st.slider('Engine Size(L)', 0.9,8.5,step=0.1, value=1.4 )
 
-    cylinder = st.selectbox('Cylinders', np.sort([ 4,  6, 12,  8, 10,  3,  5, 16]))
+
+    Transmition = st.selectbox('Transmission',np.sort(['AS5', 'M6', 'AV7', 'AS6', 'AM6', 'A6', 'AM7', 'AV8', 'AS8', 'A7',
+       'A8', 'M7', 'A4', 'M5', 'AV', 'A5', 'AS7', 'A9', 'AS9', 'AV6',
+       'AS4', 'AM5', 'AM8', 'AM9', 'AS10', 'A10', 'AV10']) )
+
+    #cylinder = st.selectbox('Cylinders', np.sort([ 4,  6, 12,  8, 10,  3,  5, 16]))
+    cylinder = st.slider('Cylinders', 3,16,step=1, value=4 )
 
     Fuel_Type = st.selectbox('Fuel Type', np.sort(['Z', 'D', 'X', 'E', 'N']) )
 
-    Fuel_city = st.number_input('Fuel Consumption City (L/100 km)', min_value = 4, max_value = 40, value = 10)
-    Fuel_hw = st.number_input('Fuel Consumption Hwy (L/100 km)', min_value = 2, max_value = 30, value = 8)
-
+    Fuel_city = st.number_input('Fuel Consumption City (L/100 km)', min_value = 4, max_value = 40, value = 8)
+    
+    
+    Fuel_hw = st.number_input('Fuel Consumption Hwy (L/100 km)', min_value = 2, max_value = 30, value = 6)
+    Fuel_comp = st.number_input('Fuel Consumption Comb (L/100 km)', min_value = 3, max_value = 35, value = 7)
     output = ""
 
 
 # Input Dict
-    input_dict = {'Vehicle Class': Vehicle_class, 'Transmission': Transmition, 'Cylinders': cylinder, 
-    'Fuel Type': Fuel_Type, 'Fuel Consumption City (L/100 km)': Fuel_city, 'Fuel Consumption Hwy (L/100 km)': Fuel_hw}
+    input_dict = {'Vehicle Class': Vehicle_class, 'Engine Size(L)':engine_size ,'Transmission': Transmition, 'Cylinders': cylinder, 
+    'Fuel Type': Fuel_Type, 'Fuel Consumption City (L/100 km)': Fuel_city, 'Fuel Consumption Hwy (L/100 km)': Fuel_hw , 'Fuel Consumption Comb (L/100 km)':Fuel_comp}
     input_df = pd.DataFrame([input_dict])
 
     # Predict
-    if st.button('Predict'):
-        output = predict_model(model, data = input_df)
-        output =  str(round(float(output['Label'][0]),2)) +  ' (g/km)'
+    # it runs without clicking the button
+    #if st.button('Predict'):
+    output = predict_model(model, data = input_df)
+    output =  str(round(float(output['Label'][0]),2)) +  ' (g/km)'
         #str(output['Label'][0])
 
     # Display
